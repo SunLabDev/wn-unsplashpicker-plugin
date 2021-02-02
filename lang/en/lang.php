b7509854cc60df06b56e3061753317af51050d07
RomainMazB <romainmazb@gmail.com>
2021-02-02 23:02:00 +0100

    first commit

---
+++ lang/en/lang.php
@@ -0,0 +1,29 @@
+<?php return [
+    'plugin' => [
+        'description' => 'Add Unsplash API to FileUpload widget'
+    ],
+
+    'permission' => [
+        'label' => 'Access and manage Unsplash credentials'
+    ],
+
+    'settings' => [
+        'description' => 'Manage Unsplash credentials',
+
+        'fields' => [
+            'api_key' => [
+                'label' => 'API Key'
+            ],
+            'api_secret' => [
+                'label' => 'API Secret'
+            ],
+        ]
+    ],
+
+    'misc' => [
+        'search' => 'Search',
+        'load_more' => 'Load more',
+        'photo_by' => 'Photo by',
+        'on' => 'on',
+    ]
+];
---
+++ lang/fr/lang.php
@@ -0,0 +1,29 @@
+<?php return [
+    'plugin' => [
+        'description' => 'Ajoute l\'API Unsplash API au widget FileUpload'
+    ],
+
+    'permission' => [
+        'label' => 'Peut accéder et modifier les identifiants Unsplash'
+    ],
+
+    'settings' => [
+        'description' => 'Gérer les identifiants',
+
+        'fields' => [
+            'api_key' => [
+                'label' => 'Clé API'
+            ],
+            'api_secret' => [
+                'label' => 'Mot de passe API'
+            ],
+        ]
+    ],
+
+    'misc' => [
+        'search' => 'Rechercher',
+        'load_more' => 'Voir plus',
+        'photo_by' => 'Photo par',
+        'on' => 'sur',
+    ]
+];
